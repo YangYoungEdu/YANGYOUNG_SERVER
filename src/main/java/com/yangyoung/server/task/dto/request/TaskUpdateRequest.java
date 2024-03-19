bef8d3faa3def5b0e3200da15876068396238d2d
dbseend <shyoon840@gmail.com>
2024-03-19 21:12:53 +0900

    [FEAT]: TASK - 할일 수정, 삭제, 날짜별 조회 구현

--- src/main/java/com/yangyoung/server/task/dto/request/TaskUpdateRequest.java
+++ src/main/java/com/yangyoung/server/task/dto/request/TaskUpdateRequest.java
@@ -1,19 +1,18 @@
 package com.yangyoung.server.task.dto.request;
 
-import com.yangyoung.server.studentTask.domain.TaskProgress;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TaskUpdateRequest {
 
     private Long taskId;
 
-    private Long studentId;
+    private String content;
 
-    private TaskProgress taskProgress;
+    private LocalDate taskDate;
 
 }
